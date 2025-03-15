d1e06827976baa475ad5303aeba39cb1adb5d479
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-03-16 01:44:00 +0800

    Add app configuration file with initial settings

---
+++ app.config.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+
+export default {
+    name: "GreenCycle",
+    slug: "GreenCycle",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/logo.png",
+    scheme: "myapp",
+    sdkVersion: "52.0.0",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    jsEngine: "hermes",
+    splash: {
+        image: "./assets/images/logo.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+    },
+    ios: {
+        supportsTablet: true,
+        jsEngine: "jsc",
+    },
+    android: {
+        adaptiveIcon: {
+            foregroundImage: "./assets/images/logo.png",
+            backgroundColor: "#ffffff",
+        },
+        package: "com.pangilinanervin22.GreenCycle",
+    },
+    web: {
+        bundler: "metro",
+        output: "static",
+        favicon: "./assets/images/logo.png",
+    },
+    plugins: ["expo-router", "expo-secure-store"],
+    experiments: {
+        typedRoutes: true,
+    },
+    extra: {
+        router: {
+            origin: false,
+        },
+        eas: {
+            projectId: "db0d206b-0b38-420e-b5d5-f499a8b090d6",
+        },
+        supabaseUrl: process.env.SUPABASE_URL,
+        supabaseKey: process.env.SUPABASE_KEY,
+        SAMPLE_ENVIRONMENT: process.env.SAMPLE_ENVIRONMENT,
+    },
+};
