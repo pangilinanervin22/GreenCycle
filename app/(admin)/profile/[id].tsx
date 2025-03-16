4ca86da90a71549e41b8c4272bb1da99d27c9caf
Tyn <103063597+ajyn06@users.noreply.github.com>
2025-03-16 09:39:28 +0800

    Admin - Edit Profile - Back Button

--- app/(admin)/profile/[id].tsx
+++ app/(admin)/profile/[id].tsx
@@ -10,6 +10,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "expo-router";
 import { useAuthStore } from "@/lib/AuthStore";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 interface ProfileFormData {
   name: string;
@@ -49,6 +50,9 @@ export default function ProfileDetails() {
   return (
     <View style={styles.container}>
       {/* Back Button */}
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+                      <FontAwesome name="arrow-left" size={20} color="#00512C" />
+                  </TouchableOpacity>
       <Text style={styles.title}>Edit Your Profile</Text>
       {/* Name Field */}
       <Text style={styles.label}>Name: </Text>
@@ -116,7 +120,22 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "#f8f9fa",
   },
+  backButton: {
+    position: 'absolute',
+    top: 40,
+    left: 15,
+    zIndex: 10,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+
+},
   title: {
+    marginTop: '20%',
     fontWeight: "bold",
     fontSize: 24,
     textAlign: "center",
